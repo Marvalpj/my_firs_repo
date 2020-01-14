0604bb9851616c7c7282222d9fb5f3e23bf6d485
pedro jose <pedro@marval.co>
2020-01-13 21:46:16 -0400

    reto

--- programacion basica/canva/reto.js
+++ programacion basica/canva/reto.js
@@ -1,5 +1,41 @@
-document.addEventListener("mousedown", dibujarMouse);
 //document.addEventListener("mouseup");
-function dibujarMouse(evento){
-    console.log(evento);
+document.addEventListener("mousedown", clickMouse);
+document.addEventListener("mouseup", noClick );
+document.addEventListener("mousemove", moviendo)
+var cuadrito = document.getElementById("areaDibujo");
+var papel = cuadrito.getContext("2d");
+
+var x;
+var y;
+
+
+var click;
+
+function clickMouse(evento){
+    click = true;
+    console.log("============evento Clic============");
+}
+function noClick(evento){
+    click = false;
+    console.log("============No click===============");
+}
+
+function moviendo(evento){
+    if( evento.target.id == "areaDibujo" && click == true){
+        x = evento.offsetX;
+        y = evento.offsetY;
+        dibujarLinea("white", x , y , x+3 , y-3 ,papel);
+    }
+}
+
+
+function dibujarLinea(color , xinicial , yinicial , xfinal , yfinal , lienzo)
+{
+    papel.beginPath();
+    papel.strokeStyle = color;
+    papel.lineWidth = 3;
+    papel.moveTo(xinicial,yinicial);
+    papel.lineTo(xfinal,yfinal)
+    papel.stroke();
+    papel.closePath();
 }
